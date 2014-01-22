14632cd4b85b851b08431339b5240687a281ee88
uymichael <michaeluy30@gmail.com>
2014-01-22 18:38:40 +0800

    added more values

--- book.sql
+++ book.sql
@@ -11,6 +11,29 @@ VALUES
 ('QA 76.9 S88 K45 2005','Systems analysis and design','Kenneth E. Kendall','9812448829','book','726 p.','on shelf','Singapore : Pearson/Prentice Hall'),
 ('QA 76 H45 1995','Discrete structures, logic and computability','James L. Hein','086720477X','book','866 p.','on shelf','Boston : Jones and Bartlett');
 
+/*MORE BOOKS*/
+INSERT INTO 'books'('call_no','title','author','isbn/issn','book_type','description','book_status','publisher') VALUES
+('QA 76.642 F67 1995' ,'Designing and building parallel programs' ,'Foster, Ian' ,'0-201-57594-9' ,'book' ,'xiii, 381 p. : ill. 24 cm' ,'ON SHELF','Reading, Mass. : Addison-Wesley, c1995' ), 
+('QA 76.642 G76 1994 ' ,'Using MPI : portable parallel programming' ,'William Gropp, Ewing Lusk, Anthony Skjellum' ,'0-262-57104-8 ' ,'book' ,'xx, 307 p. : ill. 23 cm' ,'ON SHELF','Cambridge, Mass. : MIT Press, c1994' ),
+('QA 76.642 I33 1992 ' ,'Programming environments for parallel computing' ,'Nigel Topham, Roland Ibbett, Thomas Bemmerl' ,'0-444-89764-X' ,'book' ,'ix, 244 p. : ill. 23 cm' ,'ON SHELF'   ,'Amsterdam : North-Holland, 1992' ),('QA 76.642 L47 1993 ' ,'The art of parallel programming' ,'Bruce P. Lester' ,'0-13-074980-X' ,'book' ,'xxi, 375 p. : ill. 25 cm. 1 computer disk' ,'ON SHELF'   ,'Englewood Cliffs, N.J. : Prentice Hall, c1993' ),
+('QA 76.642 R47 1992 ' ,'Research Directions in High-Level Parallel Programming Languages, France, June 17-19, 1991. : proceedings' ,'J.B. [i.e. J.P] Banatre, D. Le Metayer (eds.)' ,'3-540-55160-3' ,'book' ,'viii, 387 p. : ill. 25 cm' ,'ON SHELF'   ,'Berlin :New York : Springer-Verlag, c1992' ),
+('QA 76.642 W55 2005' ,'Parallel programming' ,'Barry Wilkinson, Michael Allen' ,'0-13-140563-2 ' ,'book' ,'467 p' ,'ON SHELF'   ,'Upper Saddle River, N.J. : Pearson Prentice Hall, c2005' ),
+('QA76.73 C153 D76 2007' ,'Data structures and algorithms in C++' ,'Drozdek, Adam' ,'978-981-4226-77-6' ,'book' ,'758 p' ,'ON SHELF'   ,'Singapore : Thomson/Course Technology, c2007' ),
+('QA 76.73 C153 S69 1994' ,'C++ and C debugging, testing and reliability' ,'Spuler, David' ,'0-13-308172-9' ,'book' ,'338 p. 1 computer disk (3.5")' ,'ON SHELF' ,'Sydney : Prentice Hall, c1994'),
+('QA76.73 J38 W44 2007' ,'Data structures and algorithm analysis in Java' ,'Weiss, Mark Allen' ,'0-321-37319-7' ,'book' ,'555 p' ,'ON SHELF'   ,'Boston : Pearson/Addison-Wesley, c2007' ),
+('QA 76.73 S67 D37 1997' ,'A guide to SQL standard' ,'Date, C. J' ,'0-201-96426-0' ,'book' ,'xxii, 522 p' ,'ON SHELF'   ,'Reading, Mass. : Addison-Wesley, c1997' ),
+('QA 76.755 F68 1993' ,'Foundations of Software Technology 13th' ,'R. K. Shyamasundar' ,'3-540-57529-4' ,'book' ,'xiv, 453 p' ,'ON SHELF'   ,'Berlin : Springer-Verlag, c1993' ),
+('QA 76.755 F68 1992' ,'Foundations of Software Technology 12th' ,'R. Shyamasundar' ,'3-540-56287-7' ,'book' ,'xiii, 403 p' ,'ON SHELF'   ,'Berlin : Springer-Verlag, c1992' ),
+('QA 76.758 A393 1993' ,'Advances in software engineering' ,'Vincenzo Ambriola, Genoveffa Tortora' ,'9-8102-1594-0' ,'book' ,'189 p' ,'ON SHELF'   ,'Singapore : World Scientific, c1993' ),
+('QA 76.758 K64 1994' ,'Data structures in Turbo Pascal' ,'Koffman, Elliot B.' ,'0-201-15624-5' ,'book' ,'xii, 593 p. ; ill. 24 cm' ,'ON SHELF'   ,'Reading, Mass. : Addison-Wesley, c1994' ),
+('QA 76.76 E95 B49 1993' ,'Knowledge engineering for information systems' ,'Beynon-Davies, Paul' ,'0-07-707586-2 ' ,'book' ,'171 p' ,'ON SHELF'   ,'London : McGraw-Hill, c1993' ),
+('0-12-178120-8' ,'Maintenance of knowledge-based systems theory' ,'Coenen, Frans' ,'0-12-178120-8 ' ,'book' ,'322 p' ,'ON SHELF'   ,'London : Academic Press, c1993' ),
+('QA 76.76 E95 E95 1992' ,'Expert Database Systems' ,'Keith Jeffery' ,'0-12-3822-55-6 ' ,'book' ,'xiv, 254 p' ,'ON SHELF'   ,'London : Academic Press, c1992' ),
+('QA 76.76 E95 F89 1992' ,'Fuzzy expert systems' ,'Abraham Kandel' ,'0-8493-4297-X ' ,'book' ,'316 p' ,'ON SHELF'   ,'	Boca Raton : CRC Press, c1992' ),
+('QA 76.76 E95 G65 1993' ,'The engineering of knowledge-based systems' ,'Gonzales, Avelino J' ,'0-13-334293-X' ,'book' ,'xx, 523 p. : ill. 25 cm. 2 computer disks' ,'ON SHELF'   ,'Englewood Cliffs, N.J.' ),
+('QA 76.76 E95 G85 1994' ,'Development of knowledge-based systems' ,'Guida, Giovanni' ,'0-471-92808-9' ,'book' ,'476 p' ,'ON SHELF'   ,'Chichester : WIley, c1994' ),
+('QA 76.76 E95 H37 1992' ,'Knowledge acquisition for expert systems' ,'Hart, Anna' ,'0-07-026911-4' ,'book' ,'196 p' ,'ON SHELF'   ,'New York : McGraw-Hill, c1992' ), ('QA 76.76 E95 H66 1993' ,'Knowledge-based systems for engineers' ,'Adrian A. Hopgood' ,'0-8493-8616-0' ,'book' ,'387 p' ,'ON SHELF','Boca Raton, Fl. : CRC Press, 1993' );
+
 
 /*BOOKS WITH NO AUTHOR*/
 INSERT INTO 'books'('call_no','title','isbn/issn','book_type','description','book_status','editor','publisher')
@@ -22,7 +45,9 @@ VALUES
 ('QA 76.9 D32 D37 1993','Database and Expert Systems Applications','3540572341','book','768 p.','on shelf','Vladimir Marik','Berlin : Springer-Verlag'),
 ('QA 76.9 D3 I56 1993','Foundations of data organization and algorithms','3540573011','book','411 p.','on shelf','David B. Lomet','Berlin : Springer-Verlag');
 
-
 /*LIBRARIAN/ADMIN*/
 INSERT INTO 'librarian'('username','password')
 VALUES('jerica','gatasnachoco');
+INSERT INTO 'librarian'('username','password')
+VALUES('michael','chocomilk');
+
