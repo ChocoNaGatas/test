2db9dd6b4d5dcc76f743ea1504118defd2d59560
Kimberly Micah Magtibay <kimberly.magtibay@gmail.com>
2014-02-10 22:19:34 +0800

    add book
    form validation

---
+++ Admin/Admin/application/views/v_index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+
+<!-- ICS Lib Index page AB-4L -->
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+	
+	<title>ICS Lib</title>
+
+	<!-- CSS Links -->
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+	<link href='<?php echo base_url(); ?>assets/css/bootstrap.css' rel="stylesheet" type="text/css" />
+    <link href='<?php echo base_url(); ?>assets/css/main.css' rel="stylesheet" type="text/css" />
+	
+</head>
+<!--end of head-->
+
+<body>
+
+
+	<div class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-collapse collapse">
+		  <a class="navbar-brand" href="#"><img src="<?php echo base_url(); ?>assets/images/ICSLibIcon.png" alt="ICSLib" length = "10" width = "70"></a> <!--If an account us active, redirect to profile instead-->
+          <ul class="nav navbar-nav">
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+		
+		  <ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+        </div> <!--/.navbar-collapse -->
+      </div>
+    </div>
+	
+	
+	<div class="container">
+		
+		
+		<div class="tabbable" id="tabs-931288">
+
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-1">
+					
+					</div>
+						<p>
+							<div class="row" id="main-content">
+								<form name='admin_login' method='post' action='<?php echo base_url('/index.php/c_librarian/')?>'>
+									<fieldset>
+									<legend> Sign In as Librarian </legend>
+									<br/>
+									<p>
+										<input type='text' name='uname' placeholder='Username' required/><br/>
+									</p>
+									<p>
+										<input type='password' name='pword' placeholder='Password' required/><br/>
+									</p>
+									<br/>
+									<input type="submit" name="submit" id="submit" class="btn btn-primary" value="Log In"/>
+									</fieldset>
+								</form>
+							</div>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+	</div>
+        
+	
+	
+	
+
+
+	
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+
+	
+</body>
+<!--end of body-->
+
+</html>
+<!--end of v_index-->
